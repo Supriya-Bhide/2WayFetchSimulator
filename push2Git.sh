5278e48868c0052cc1f738c30fd48e19e34a979c
Supriya-Bhide <bsupriyanandubhide@gmail.com>
2022-11-30 19:52:14 +0530

    test

---
+++ push2Git.sh
@@ -0,0 +1,12 @@
+echo $1
+if [ -z "$1" ]
+then
+	echo "Enter in the following format:"
+	echo "./push2Git.sh < message >"
+	exit
+fi
+
+git remote add origin_name https://github.com/Supriya-Bhide/2WayFetchSimulator.git
+git add .
+git commit -m "$1"
+git push -u origin_name main
