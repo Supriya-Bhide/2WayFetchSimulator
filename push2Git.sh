f1f5227053a13c8960521c6d16fc5da2e0df2930
Supriya-Bhide <bsupriyanandubhide@gmail.com>
2022-11-30 19:57:29 +0530

    2WayFetchSimulator

--- push2Git.sh
+++ push2Git.sh
@@ -1,13 +1,14 @@
 echo "your wish is my command"
 echo $1
-if [ -z "$1" ]
+echo $2
+if [ -z "$1" ] | [ -z "$2" ]
 then
 	echo "Enter in the following format:"
-	echo "./push2Git.sh < message >"
+	echo "./push2Git.sh <reponame> < message >"
 	exit
 fi
 
-git remote add origin_name https://github.com/Supriya-Bhide/2WayFetchSimulator.git
+git remote add origin_name https://github.com/Supriya-Bhide/$2.git
 git add .
 git commit -m "$1"
 git push -u origin_name main
